fb1a348864744086dc853fc6292bfb57fa911d88
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2023-01-30 02:47:12 -0300

    teste husky 3

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,6 @@ import App from './App'
 
 test('renders learn react link - teste husky 2', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
+  const linkElement = screen.getByText(/learn react1/i)
   expect(linkElement).toBeInTheDocument()
 })
